9d2aa6a472c2b14ecbd72d9a1d50b44da5decfa5
luiz-oliveira <lcpojr2@gmail.com>
2018-09-13 14:33:11 -0700

    Initial commit

--- customer_service/apps/customers/tests.py
+++ customer_service/apps/customers/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, Client, RequestFactory
 from django.urls import reverse
 from .models import Customer
 from faker import Faker
@@ -14,27 +14,27 @@ class ReadCustomerTestCase(TestCase):
         return reverse('customers:customer-detail', args=[self.customer.id])
 
     def setUp(self):
-        faker = Faker()
+        self.faker = Faker()
+        self.client = Client()
+        self.factory = RequestFactory()
 
-        # Creating a Default user for test
-        User.objects.create_superuser('username', 'user@email.com', 'password').save()
-
-        # Login user
-        self.client.login(username='john', password='johnpassword')
+        # Creating a super_user
+        self.super_user = User.objects.create_superuser('username', 'user@email.com', 'password').save()
 
         # Creating a basic customer
         self.customer = Customer()
-        self.customer.full_name = faker.name()
-        self.customer.address = faker.address()
+        self.customer.full_name = self.faker.name()
+        self.customer.address = self.faker.address()
         self.customer.cpf = '99999999999'
         self.customer.email = 'test@test.com'
         self.customer.phone = '5524988457899'
         self.customer.save()
 
-    def test_can_read_customer_list(self):
-        response = self.client.get(self.list_url())
-        self.assertEqual(response.status_code, 200)
 
-    def test_can_read_user_detail(self):
-        response = self.client.get(self.detail_url())
-        self.assertEqual(response.status_code, 200)
+    def test_cannot_read_customer_list_whitout_token(self):
+        response = self.client.post(self.list_url())
+        self.assertEqual(response.status_code, 401)
+
+    def test_cannot_read_customer_detail_whitout_token(self):
+        response = self.client.post(self.list_url())
+        self.assertEqual(response.status_code, 401)
