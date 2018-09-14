f2d8cf2de6c0551511ef89e3c37987122ff34c9c
luiz-oliveira <lcpojr2@gmail.com>
2018-09-14 05:51:26 -0700

    adding tests

--- customer_service/apps/customers/tests.py
+++ customer_service/apps/customers/tests.py
@@ -17,10 +17,7 @@ class CustomerTestCase(APITestCase):
         self.faker = Faker()
 
         self.csrf_client = APIClient(enforce_csrf_checks=True)
-        self.username = 'user'
-        self.email = 'user@gmail.com'
-        self.password = 'password'
-        self.user = User.objects.create_user(self.username, self.email, self.password)
+        self.user = User.objects.create_user('user', 'user@gmail.com', 'password')
 
         # Creating a application
         self.app = Application.objects.create(
@@ -40,6 +37,16 @@ class CustomerTestCase(APITestCase):
             application=self.app
         )
 
+        # Criating a default customer
+        self.customer = Customer()
+        self.customer.full_name = self.faker.name()
+        self.customer.cpf = "98765432198"
+        self.customer.email = "customer@customer.com"
+        self.customer.phone = "552487996633"
+        self.customer.address = self.faker.address()
+        self.customer.save()
+        
+
     def _create_authorization_header(self, token=None):
         return "Bearer {0}".format(token or self.access_token.token)
 
@@ -56,3 +63,30 @@ class CustomerTestCase(APITestCase):
         
         response = self.csrf_client.post(url, data=payload, HTTP_AUTHORIZATION=header)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_can_list_customers(self):
+        header = self._create_authorization_header()
+        url = reverse("customers:customers-list")       
+        response = self.csrf_client.get(url, HTTP_AUTHORIZATION=header)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_can_detail_a_customer(self):
+        header = self._create_authorization_header()
+        url = reverse("customers:customers-detail", args=[self.customer.pk])       
+        response = self.csrf_client.get(url, HTTP_AUTHORIZATION=header)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_can_update_a_customer(self):
+        header = self._create_authorization_header()
+        url = reverse("customers:customers-list")
+        payload = {"email": "test2@email.com"}
+
+        url = reverse("customers:customers-detail", args=[self.customer.pk])       
+        response = self.csrf_client.patch(url, HTTP_AUTHORIZATION=header)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_can_delete_a_customer(self):
+        header = self._create_authorization_header()
+        url = reverse("customers:customers-detail", args=[self.customer.pk])       
+        response = self.csrf_client.delete(url, HTTP_AUTHORIZATION=header)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
--- customer_service/apps/users/tests.py
+++ customer_service/apps/users/tests.py
@@ -1,23 +1,44 @@
-from django.test import TestCase, Client, RequestFactory
+from rest_framework.test import APITestCase
+from rest_framework import status
+from oauth2_provider.models import Application, AccessToken
+from rest_framework.test import APIRequestFactory, APIClient
 from django.urls import reverse
 from django.contrib.auth.models import User
+from datetime import datetime, timedelta
 
-class ReadCustomerTestCase(TestCase):
 
-    def list_url(self):
-        return reverse('users:user-list')
 
-    def detail_url(self):
-        return reverse('users:user-detail', args=[self.customer.id])
+class CustomerTestCase(APITestCase):
 
     def setUp(self):
-        self.client = Client()
-        self.factory = RequestFactory()
+        self.factory = APIRequestFactory()
 
-    def test_cannot_read_customer_list_whitout_token(self):
-        response = self.client.post(self.list_url())
-        self.assertEqual(response.status_code, 401)
+        self.csrf_client = APIClient(enforce_csrf_checks=True)
+        self.user = User.objects.create_user('user', 'user@gmail.com', 'password')
 
-    def test_cannot_read_customer_detail_whitout_token(self):
-        response = self.client.post(self.list_url())
+        # Creating a application
+        self.app = Application.objects.create(
+            client_type=Application.CLIENT_CONFIDENTIAL,
+            authorization_grant_type=Application.GRANT_PASSWORD,
+            redirect_uris='',
+            name='dummy',
+            user=self.user
+        )
+
+        # Criating the access token
+        self.access_token = AccessToken.objects.create(
+            user=self.user,
+            scope='read write',
+            expires=datetime.now() + timedelta(seconds=300),
+            token='secret-access-token-key',
+            application=self.app
+        )
+        
+
+    def _create_authorization_header(self, token=None):
+        return "Bearer {0}".format(token or self.access_token.token)
+
+    def test_cannot_access_customer_endpoints_whitout_token(self):
+        url = reverse("customers:customers-detail", args=[self.user.pk])  
+        response = self.client.post(url)
         self.assertEqual(response.status_code, 401)
